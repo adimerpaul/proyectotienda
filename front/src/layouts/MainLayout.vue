f9f4f024ba64f9a60faa2fca3a7bacc7b8a9fae9
adimerpaul <adimer101@gmail.com>
2023-11-25 20:24:02 -0400

    Modificado el proyecto para recuperacion de datos del back

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,44 +1,22 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
+    <q-header>
+      <q-toolbar class="bg-white text-black">
+        <div>
+          <div class="text-subtitle2 text-bold">Descubre</div>
+          <div class="text-grey text-caption">Busca y encuentra lo que necesitas</div>
+        </div>
+
+        <q-space />
+
+        <q-btn-toggle
+          outline
+          v-model="leftDrawerOpen"
           flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+          :options="options"
         />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -47,65 +25,17 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
   setup () {
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
+      options: [
+        { icon: 'search', label: '', value: 'search' },
+        { icon: 'notifications', label: '', value: 'notifications' }
+      ],
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -1,26 +1,104 @@
 <template>
   <q-page>
-    <pre>{{productos}}</pre>
+    <q-carousel
+      v-model="slide"
+      swipeable
+      animated
+      infinite
+      navigation
+      padding
+      arrows
+      height="300px"
+      class="text-white"
+    >
+      <q-carousel-slide :name="1" class="column no-wrap flex-center" style="background: url('./images/fondo1.jpg') no-repeat center center; background-size: cover;">
+        <q-icon name="style" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide :name="2" class="column no-wrap flex-center" style="background: grey no-repeat center center; background-size: cover;" >
+        <q-icon name="live_tv" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide :name="3" class="column no-wrap flex-center" style="background: grey no-repeat center center; background-size: cover;" >
+        <q-icon name="layers" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide :name="4" class="column no-wrap flex-center" style="background: grey no-repeat center center; background-size: cover;" >
+        <q-icon name="terrain" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+    <q-virtual-scroll
+      :items="heavyList"
+      virtual-scroll-horizontal
+    >
+      <q-chip outline color="black" text-color="white" v-for="categoria in categorias" :key="categoria.id">
+        {{ categoria.nombre }}
+      </q-chip>
+    </q-virtual-scroll>
+    <div>
+      <div class="text-subtitle2 text-bold q-pl-lg">Venta express</div>
+    </div>
+    <div>
+      <q-virtual-scroll
+        :items="heavyList"
+        virtual-scroll-horizontal
+      >
+        <q-card class="q-ma-md" style="width: 150px;" v-for="producto in productos" :key="producto.id">
+          <q-card-section class="q-pa-none">
+            <q-img :src="`http://localhost:8000/images/${producto.imagen}`" />
+          </q-card-section>
+          <q-card-section class="q-pa-md">
+            <div class="text-h6 text-bold">{{ producto.nombre }}</div>
+            <div class="text-subtitle2 text-bold">{{ producto.precio }}</div>
+          </q-card-section>
+        </q-card>
+      </q-virtual-scroll>
+    </div>
+<!--    <pre>{{productos}}</pre>-->
+<!--    <pre>{{categorias}}</pre>-->
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-
 export default defineComponent({
   name: 'IndexPage',
   data () {
     return {
-      productos: []
+      slide: 1,
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris',
+      productos: [],
+      categorias: [],
+      heavyList: []
     }
   },
   mounted () {
     this.getProductos()
+    this.getCategorias()
+    for (let i = 0; i < 1; i++) {
+      this.heavyList.push({
+        label: 'Option ' + (i + 1),
+        class: i % 2 === 0 ? 'q-pa-md self-center bg-grey-2 text-black' : 'q-pa-lg bg-black text-white'
+      })
+    }
   },
   methods: {
     async getProductos () {
       const response = await this.$axios.get('http://localhost:8000/api/products')
       this.productos = response.data
+    },
+    async getCategorias () {
+      const response = await this.$axios.get('http://localhost:8000/api/categories')
+      this.categorias = response.data
     }
   },
 })
